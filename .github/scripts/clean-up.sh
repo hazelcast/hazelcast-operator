a69adcaaf67fbd785f8d56070aa8b2ac0477363c
Muhammet Yazici <muhammet.yazici@boun.edu.tr>
2020-12-11 20:08:35 +0300

    Update custom resource definitions and Remove delete commands from clean-up.sh
    Also did some refactoring

--- .github/scripts/clean-up.sh
+++ .github/scripts/clean-up.sh
@@ -10,9 +10,6 @@ PROJECT=$2
 oc delete -f ${WORKDIR}/hazelcast.yaml
 oc delete -f ${WORKDIR}/secret.yaml
 oc delete -f ${WORKDIR}/operator-rhel.yaml
-oc delete -f ${WORKDIR}/hazelcastcluster.crd.yaml
-oc delete -f ${WORKDIR}/hazelcast-rbac.yaml
-oc delete -f ${WORKDIR}/operator-rbac.yaml
 oc delete secret pull-secret
 oc delete project $PROJECT
 
